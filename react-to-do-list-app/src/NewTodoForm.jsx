b9e65e21a7f91331ee18ff18075da7aea5350d3d
JustinParviz <jarshravan@gmail.com>
2025-04-03 15:22:47 -0400

    Separated the list component from the App and created a new component called TodoList

--- react-to-do-list-app/src/NewTodoForm.jsx
+++ react-to-do-list-app/src/NewTodoForm.jsx
@@ -6,17 +6,24 @@ export function NewTodoForm({ onSubmit }) {
     function handleSubmit(e) {
         e.preventDefault()
         if (newItem === "") return
-    
+
         onSubmit(newItem)
-    
+
         setNewItem("")
-      }
+    }
 
     return (
-        <form onSubmit={handleSubmit} className="new-item-form">
+        <form
+            onSubmit={handleSubmit}
+            className="new-item-form">
             <div className="form-row">
                 <label htmlFor="item">New Item</label>
-                <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item" />
+                <input
+                    value={newItem}
+                    onChange={e => setNewItem(e.target.value)}
+                    type="text"
+                    id="item"
+                />
             </div>
             <button className="btn">Add</button>
         </form>
